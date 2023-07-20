e8390575744281a4e5df63e7f2a7b05c929c9e74
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-07-19 19:07:19 -0600

    Did exercise of 16th video

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,44 @@
+(() => {
+	// función para obtener información de una película por Id
+	function getMovieById(id: string) {
+		console.log({ id });
+	}
+
+	// función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+	function getMovieCastById(id: string) {
+		console.log({ id });
+	}
+
+	// funcion para obtener el bio del actor por el id
+	function getActorBioById(id: string) {
+		console.log({ id });
+	}
+
+	interface NewMovie {
+		title: string;
+		description: string;
+		rating: number;
+		cast: Array<string>;
+	}
+
+	// Crear una película
+	function createMovie({ title, description, rating, cast }: NewMovie) {
+		console.log({ title, description, rating, cast });
+	}
+
+	interface NewActor {
+		fullName: string;
+		birthdate: Date;
+	}
+
+	// Crea un nuevo actor
+	function createActor({ fullName, birthdate }: NewActor): boolean {
+		// tarea asincrona para verificar nombre
+		// ..
+		// ..
+		if (fullName === "fernando") return false;
+
+		console.log("Crear actor");
+		return true;
+	}
+})();
